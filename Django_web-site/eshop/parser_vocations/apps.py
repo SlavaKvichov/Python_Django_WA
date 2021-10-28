16a2ee700ede4180ec9ce77d95c7d26b7ab725e7
SlavaKvichov <92995374+SlavaKvichov@users.noreply.github.com>
2021-10-28 21:13:51 +0300

    Delete Django_web-site/eshop directory

--- Django_web-site/eshop/parser_vocations/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ParserVocationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'parser_vocations'
--- Django_web-site/eshop/weather/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.weather),
-    path('result/', views.weather_result)
-]
